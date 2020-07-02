73204e3358d0a65839973c2a20f0479fc501e3ff
sjppp510 <43102975+sjppp510@users.noreply.github.com>
2020-07-03 00:14:17 +0900

    Update Erin.py

--- Erin.py
+++ Erin.py
@@ -345,7 +345,7 @@ async def on_message(message):
         await message.channel.send(message.author.mention + "님의 ID는 \n" + "```" + str(message.author.id) + "```")
         return None
     
-    if talk.startswith("삭제") or talk.startswith("청소"):
+    if talk.startswith("삭제") or talk.startswith("청소") or talk.startswith(""):
         utcnow = datetime.datetime.utcnow()
         try:
             time_gap = datetime.timedelta(hours=int(talk.split(" ")[1]))
