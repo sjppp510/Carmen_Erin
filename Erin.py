70204101366e52655b8b4ef3c8ab5e13abe04a09
sjppp510 <43102975+sjppp510@users.noreply.github.com>
2020-07-09 14:08:24 +0900

    Update Erin.py

--- Erin.py
+++ Erin.py
@@ -698,12 +698,14 @@ async def on_voice_state_update(member, before, after):
             time.sleep(2)
             if after.channel.name == "check in":
                 if member in discord.utils.get(client.get_all_channels(), guild__name=member.guild.name, name="check in").members:
-                    newChannel = await after.channel.category.create_voice_channel("제목을 입력해주세요.")
+                    newChannel = await member.guild.create_voice_channel("제목을 입력해주세요.")
+                    await newChannel.edit(category=after.channel.category)
                     await newChannel.set_permissions(member, manage_channels=True)
                     await member.move_to(newChannel)
             elif after.channel.name == "private check in":
                 if member in discord.utils.get(client.get_all_channels(), guild__name=member.guild.name, name="private check in").members:
-                    newChannel = await after.channel.category.create_voice_channel(name="제목을 입력해주세요.")
+                    newChannel = await member.guild.create_voice_channel("제목을 입력해주세요.")
+                    await newChannel.edit(category=after.channel.category)
                     await newChannel.set_permissions(member.guild.get_role(629963963446198292), view_channel=False)
                     await newChannel.set_permissions(member, manage_channels=True)
                     await member.move_to(newChannel)
