4be678dbfdc384ba25dd713a67b902d77245ea46
sjppp510 <43102975+sjppp510@users.noreply.github.com>
2020-07-27 22:54:53 +0900

    Update Erin.py

--- Erin.py
+++ Erin.py
@@ -391,7 +391,7 @@ async def on_message(message):
         return None
     
     if talk.startswith("프로필"):
-        await message.channel.send(format=message.author.avatar_url_as("png"))
+        await message.channel.send(message.author.avatar_url_as(format="png"))
         return None
 
     if talk.startswith("sns"):
