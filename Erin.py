6eb1ed34617b225a04e47c7269374669c051e332
sjppp510 <43102975+sjppp510@users.noreply.github.com>
2020-07-27 20:50:22 +0900

    Update Erin.py

--- Erin.py
+++ Erin.py
@@ -485,7 +485,7 @@ async def on_message(message):
             pass
         return None
     
-    if talk.startswith("서버링크") or talk.startswith("링크") or talk.startswith("초대") or:
+    if talk.startswith("서버링크") or talk.startswith("링크") or talk.startswith("초대"):
         await message.channel.send(message.guild.create_invite(max_age=0, max_uses=0, reason="하바유보"))
         return None
 
