3b13acae30aaa9be3bd592b072a9c8993a3dd262
sjppp510 <43102975+sjppp510@users.noreply.github.com>
2020-07-28 23:08:35 +0900

    Update Erin.py

--- Erin.py
+++ Erin.py
@@ -860,11 +860,8 @@ async def Reaction(payload, user, msg, tf):
                     if tf:
                         count = 0
                         for c in msg.reactions:
-                            print(payload.emoji.name)
-                            print(c.emoji)
-                            print(payload.emoji.name == c.emoji)
                             if payload.emoji.name == c.emoji:
-                                count = c.count + len(message.guild.get_role(731802943011160165).members)
+                                count = c.count + len(msg.guild.get_role(731802943011160165).members)
                                 print(count)
                         if role in user.roles:
                             return None
