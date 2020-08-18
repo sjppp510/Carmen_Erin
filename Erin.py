a4c6b99ce1793cfa2b89ed36f8d628dc6f62e8e3
sjppp510 <43102975+sjppp510@users.noreply.github.com>
2020-08-18 11:09:53 +0900

    Update Erin.py

--- Erin.py
+++ Erin.py
@@ -67,8 +67,9 @@ async def on_message(message):
     talk = message.content[len(prefix):]
 
     if talk.startswith("재시작"):
+        access_token = os.environ["BOT_TOKEN"]
         await client.close()
-        await client.start()
+        await client.start(token=access_token)
         await message.channel.send("재시작 되었습니다.")
     if talk.startswith("초기화"):
         utcnow = datetime.datetime.utcnow()
