51261a99d63061010d43017bddefaa30c51b566a
sjppp510 <43102975+sjppp510@users.noreply.github.com>
2020-08-30 20:39:24 +0900

    Update Erin.py

--- Erin.py
+++ Erin.py
@@ -72,10 +72,10 @@ async def on_message(message):
         return None
 
             
-    if not (message.content.startswith(prefix)) or not(message.content.startswith(prefix2)):
+    if not(message.content.startswith(prefix)) or not(message.content.startswith(prefix2)):
         return None
 
-    talk = message.content[len(prefix):]
+    talk = message.content.split(" ")[1]
 
     if talk.startswith("재시작"):
         os.system("python Erin.py")
